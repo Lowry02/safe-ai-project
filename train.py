9b15edea4622a9b6d231c24931582286141ddb72
JackSerafini <lagiacromo2803@gmail.com>
2026-01-21 18:31:25 +0100

    First part of the model, to be completed

---
+++ train.py
@@ -0,0 +1,24 @@
+from tqdm import tqdm
+
+def train(model, device, train_loader, optimizer, epoch, criterion):
+    
+    model.train()
+    loss_total = 0
+    loop = tqdm(train_loader, leave = True)
+
+    for batch_idx, (data, target) in enumerate(loop):
+        
+        data = data.to(device)
+        target = target.to(device)
+        
+        output = model(data)
+        
+        loss = criterion(output, target)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        loss_total += loss.item()
+
+    return loss_total/batch_idx
