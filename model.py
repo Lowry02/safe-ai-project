12f9fccf505eecea421b107b60179619f945586f
JackSerafini <lagiacromo2803@gmail.com>
2026-01-22 12:25:19 +0100

    Done with the model (to be improved), missing contrastive loss

--- model.py
+++ model.py
@@ -2,17 +2,17 @@ import torch
 import torch.nn as nn
 
 class Net(nn.Module):
-    def __init__(self, input_dim = 3, num_classes = 10):
-        super().__init__()
+    def __init__(self, in_channels = 3, num_classes = 10):
+        super(Net, self).__init__()
 
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(kernel_size=2),
 
             nn.Conv2d(64, 192, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(kernel_size=2),
 
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(),
@@ -22,7 +22,7 @@ class Net(nn.Module):
 
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(kernel_size=2)
         )
 
         self.classifier = nn.Sequential(
