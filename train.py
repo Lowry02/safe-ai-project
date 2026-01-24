200be216d2732ff476d407eb42f65281e69f887e
Lowry02 <lorenzocusin02@gmail.com>
2026-01-24 16:37:23 +0100

    Organizing training objects

--- train.py
+++ train.py
@@ -1,24 +1,60 @@
-from tqdm import tqdm
+import torch
+import torch.nn as nn
+from torch.optim import Optimizer
+from typing import Callable, Tuple
+from torch.utils.data import DataLoader
 
-def train(model, device, train_loader, optimizer, epoch, criterion):
+
+def train(
+    model:nn.Module,
+    train_loader:DataLoader,
+    validation_loader:DataLoader,
+    optimizer:Optimizer,
+    criterion:nn.Module,
+    epochs:int,
+    device:torch.device|str,
+    middleware:Callable[[torch.Tensor, torch.Tensor], tuple[torch.Tensor, torch.Tensor]] | None = None
+) -> nn.Module:
     
-    model.train()
-    loss_total = 0
-    loop = tqdm(train_loader, leave = True)
+    def default_middleware(images:torch.Tensor, labels:torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        return images, labels
+    
+    if middleware is None:
+        middleware = default_middleware
+        
+    for epoch in range(epochs):
+        model.train()
+        train_loss = 0
+        for _, (images, labels) in enumerate(train_loader):
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            images, labels = middleware(images, labels)
 
-    for batch_idx, (data, target) in enumerate(loop):
-        
-        data = data.to(device)
-        target = target.to(device)
-        
-        output = model(data)
-        
-        loss = criterion(output, target)
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            output = model(images)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
 
-        loss_total += loss.item()
+        train_loss /= len(train_loader)
 
-    return loss_total/batch_idx
+        model.eval()
+        with torch.no_grad():
+            validation_loss = 0
+            for images, labels in validation_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+                output = model(images)
+                loss = criterion(output, labels)
+                validation_loss += loss.item()
+
+        validation_loss /= len(validation_loader)
+
+        print(f"> Epoch {epoch+1}/{epoch}")
+        print(f"  Train Loss: {train_loss:.4f}")
+        print(f"  Validation Loss: {validation_loss:.4f}")
+        
+    return model
+
